8692f510032ba09b4b99e53c371fc20f63011101
zhaopengyun <zpy94254@gmail.com>
2022-09-22 18:22:36 +0800

    fix

--- filter/geoip.go
+++ filter/geoip.go
@@ -15,6 +15,7 @@ import (
 var Geoip2Reader *geoip2.Reader
 var Geoip2Reader1 *geoip2.Reader
 var once sync.Once
+var once1 sync.Once
 
 type GeoIPFilter struct {
 	config   map[interface{}]interface{}
@@ -38,7 +39,7 @@ func GetGeoIPLib(database string) *geoip2.Reader {
 func GetGeoIPLibIsp(database string) *geoip2.Reader {
 	var err error
 
-	once.Do(func() {
+	once1.Do(func() {
 		Geoip2Reader1, err = geoip2.Open(database)
 		if err != nil {
 			log.Fatal(err.Error())
@@ -68,13 +69,16 @@ func newGeoIPFilter(config map[interface{}]interface{}) topology.Filter {
 }
 
 func (plugin *GeoIPFilter) Filter(event map[string]interface{}) (map[string]interface{}, bool) {
+
 	ipname, ok := plugin.config[plugin.target].(string)
 	glog.V(5).Infof("GeoIP Filter %s %+v %+v", ipname, plugin.config, event)
 	if !ok {
+		glog.Errorf("get ipname %s", plugin.target)
 		return event, true
 	}
 	ip, ok := event[ipname].(string)
 	if !ok {
+		glog.Errorf("get ip %+v", event)
 		return event, true
 	}
 	glog.V(5).Infof("GeoIP Filter_ip %s %+v %s", ipname, plugin.config, ip)
@@ -85,7 +89,9 @@ func (plugin *GeoIPFilter) Filter(event map[string]interface{}) (map[string]inte
 		return event, true
 	}
 
-	record, err := GetGeoIPLib(plugin.database).City(ipAddr)
+	gg := GetGeoIPLib(plugin.database)
+
+	record, err := gg.City(ipAddr)
 	if err != nil {
 		return event, true
 	}
