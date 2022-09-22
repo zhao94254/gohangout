59bea7354f95ef370a6d49399b4f6fc57babb4f7
zhaopengyun <zpy94254@gmail.com>
2022-09-22 15:56:06 +0800

    feat: + geo ip

--- filter/geoip.go
+++ filter/geoip.go
@@ -20,11 +20,11 @@ type GeoIPFilter struct {
 	database string
 }
 
-func GetGeoIPLib() *geoip2.Reader {
+func GetGeoIPLib(database string) *geoip2.Reader {
 	var err error
 
 	once.Do(func() {
-		Geoip2Reader, err = geoip2.Open("./GeoIP2-City.mmdb")
+		Geoip2Reader, err = geoip2.Open(database)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
@@ -40,6 +40,10 @@ func newGeoIPFilter(config map[interface{}]interface{}) topology.Filter {
 	plugin := &GeoIPFilter{
 		config: config,
 		target: "geoip",
+		database: "./GeoIP2-City.mmdb",
+	}
+	if database, ok := config["database"]; ok {
+		plugin.database = database.(string)
 	}
 
 	return plugin
@@ -51,21 +55,36 @@ func (plugin *GeoIPFilter) Filter(event map[string]interface{}) (map[string]inte
 		return event, true
 	}
 	ipAddr := net.ParseIP(ip)
-	record, err := GetGeoIPLib().City(ipAddr)
+	isp, err := GetGeoIPLib(plugin.database).ISP(ipAddr)
 	if err != nil {
-		return nil, true
+		return event, true
 	}
 
-	event["country"] = record.Country.Names["en"]
+	record, err := GetGeoIPLib(plugin.database).City(ipAddr)
+	if err != nil {
+		return event, true
+	}
+
+	event["country_name"] = record.Country.Names["en"]
 	event["country_code"] = record.Country.IsoCode
 	if len(record.Subdivisions) > 0 {
-		event["region"] = record.Subdivisions[0].Names["en"]
+		event["region_name"] = record.Subdivisions[0].Names["en"]
 	} else {
-		event["region"] = ""
+		event["region_name"] = ""
 	}
-	event["city"] = record.City.Names["en"]
-	event["lat"] = record.Location.Latitude
-	event["lon"] = record.Location.Longitude
+	event["city_name"] = record.City.Names["en"]
+
+
+
+	event["location"] = map[string]interface{}{
+		"lat": record.Location.Latitude,
+		"lon": record.Location.Longitude,
+	}
+	event["isp"] = isp.ISP
+	event["organization"] = isp.Organization
+
+
+
 
 	return event, true
 }
